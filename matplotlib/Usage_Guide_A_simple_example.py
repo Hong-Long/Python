17647a1d939db1b53b0f64a44afb5ee2e6a84bfa
Hong-Long <whl1457139188@163.com>
2020-06-22 10:44:26 +0800

    Modify the wrong word about the "sama"

--- matplotlib/Usage_Guide_A_simple_example.py
+++ matplotlib/Usage_Guide_A_simple_example.py
@@ -2,5 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 fig,ax = plt.subplots()         # Create a figure containing a single axes.
-ax.plot([1,2,3,4],[1,4,2,3])    # Plot soma data on the axes
-plt.show()                      # Show the figure 
+ax.plot([1,2,3,4],[1,4,2,3])    # Plot some data on the axes
+plt.show()                      # Show the figure
+
