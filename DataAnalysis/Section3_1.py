67a120390cc1eb48d1268e8be547ab3a002357a8
HongLong <whl1457139188@163.com>
2020-07-10 06:54:42 +0800

    数据分析第三章第一节

--- DataAnalysis/Section3_1.py
+++ DataAnalysis/Section3_1.py
@@ -8,6 +8,7 @@
 """
 import bisect
 
+
 def tup():
     tup = 4, 5, 6  # create the tuple
     print('The first way to create the tuple,\r\n tup = ', tup)  # printf the result
@@ -63,13 +64,15 @@ def tup():
     a, b, *_ = values
     print('a={0},b={1}'.format(a, b, ))
 
+
 """
     Part02: list ：列表
 """
 
+
 def List():
-    a_list = [2,3,7,None]   # create a list
-    tup = ('foo','bar','baz')  # create a tuple
+    a_list = [2, 3, 7, None]  # create a list
+    tup = ('foo', 'bar', 'baz')  # create a tuple
     b_list = list(tup)  # change tup to list by using list function
     print('b_list={0}'.format(b_list))  # create the result
     b_list[1] = 'peekaboo'  # change the element about the second element
@@ -79,7 +82,7 @@ def List():
     b_list.append('dwarf')  # add an element to the end of the list
     print('b_list={0}'.format(b_list))  # create the result
 
-    b_list.insert(1,'red')  # insert the element to the specified list position. Computationally more expensive
+    b_list.insert(1, 'red')  # insert the element to the specified list position. Computationally more expensive
     print('b_list={0}'.format(b_list))  # create the result
 
     b_list.pop(2)  # remove the element at a specific position and return
@@ -90,29 +93,29 @@ def List():
     b_list.remove('dwarf')
     print('b_list={0}'.format(b_list))  # create the result
 
-    print('list={0}'.format([4,None,'foo']+[7,8,(2,3)]))  # two lists can be connected using +
+    print('list={0}'.format([4, None, 'foo'] + [7, 8, (2, 3)]))  # two lists can be connected using +
 
-    b_list.extend([8,9,(2,3),('fjo')])  # Use the extend method to add multiple elements to the list
+    b_list.extend([8, 9, (2, 3), ('fjo')])  # Use the extend method to add multiple elements to the list
     print('b_list={0}'.format(b_list))  # create the result
 
-    a = [7,2,5,1,3]
+    a = [7, 2, 5, 1, 3]
     a.sort()
     print('a={0}'.format(a))
 
-    c = [1,2,2,2,3,4,7]
+    c = [1, 2, 2, 2, 3, 4, 7]
 
-    print('bisect.bisect(c,2)={0}'.format(bisect.bisect(c,2)))
-    print('bisect.bisect(c,5)={0}'.format(bisect.bisect(c,5)))
-    print('bisect.bisect(c,6)={0}'.format(bisect.bisect(c,6)))
+    print('bisect.bisect(c,2)={0}'.format(bisect.bisect(c, 2)))
+    print('bisect.bisect(c,5)={0}'.format(bisect.bisect(c, 5)))
+    print('bisect.bisect(c,6)={0}'.format(bisect.bisect(c, 6)))
     print('c={0}'.format(c))
     print('bisect.insort(c,6)={0}'.format(bisect.insort(c, 6)))
     print('c={0}'.format(c))
 
     # Slice
-    seq = [7,2,3,7,5,6,0,1]
+    seq = [7, 2, 3, 7, 5, 6, 0, 1]
     print('seq={0}'.format(seq))
     print('seq[1:5]={0}'.format(seq[1:5]))
-    seq[3:4] = [6,3]
+    seq[3:4] = [6, 3]
     print('seq={0}'.format(seq))
     print('seq[:5]={0}'.format(seq[:5]))
     print('seq[-4:]={0}'.format(seq[-4:]))
@@ -120,24 +123,58 @@ def List():
     print('seq[::-1]={0}'.format(seq[::-1]))
     # 内建序列函数
     # emumerate 用于遍历一个序列，返回i和value 其中i是索引，value为对应的值
-    some_list =['foo','bar','baz']
-    mapping={}
-    for i,v in enumerate(some_list):
-        mapping[v]=i
+    some_list = ['foo', 'bar', 'baz']
+    mapping = {}
+    for i, v in enumerate(some_list):
+        mapping[v] = i
         print('mapping = {0}'.format(mapping))
     # sorted
     print('排列后的数据为{0}'.format(sorted('horse race')))
 
     # Zip
-    print('Zip result is {0}'.format(list(zip(['foo','bar','baz'],['one','two','three'],[False,True]))))
-    for i,(a,b) in enumerate(zip(['foo','bar','baz'],['one','two','three'])):
-        print('{0}:{1},{2}'.format(i,a,b))
+    print('Zip result is {0}'.format(list(zip(['foo', 'bar', 'baz'], ['one', 'two', 'three'], [False, True]))))
+    for i, (a, b) in enumerate(zip(['foo', 'bar', 'baz'], ['one', 'two', 'three'])):
+        print('{0}:{1},{2}'.format(i, a, b))
+    # reversed 将序列中的元素倒叙排列
+    print(list(sorted(range(10))))
+    print(list(reversed(range(10))))
 
 
+"""
+    Part02: dict ：列表，又称哈希表或者关联数组
+"""
 
+
+def Dict():
+    dict1 = {'a': 'some value', 'b': '[1,2,3,4]','d':'other values','e':'values'}  # create a dictionary
+    print('dict = {0}'.format(dict1))  # printf the result
+
+    # insert one element to an exited dictionary
+    dict1['c'] = ('a', 'b', 'c', 'd')
+    print('after added, dict ={0}'.format(dict1))
+    # index the values in the dictionary by key value
+    print('dict1_b= {0}'.format(dict1['b']))
+
+    # Del the value by del method
+    del dict1['c']
+    print('dict = {0}'.format(dict1))
+    # del the value by pop method
+    ret = dict1.pop('b')
+    print('ret = {0},dict={1}'.format(ret, dict1))
+    # printf the key and values
+    print('key ={0},value={1}'.format(dict1.keys(),dict1.values()))
+
+    # Merge dictionary by using update
+    dict1.update({'f':'foo','e':12})
+    print('dict={0}'.format(dict1))
+
+    # Generate a dictionary from a sequence
+    mapping= dict(zip(range(10),reversed(range(10))))
+    print('mapping={0}'.format(mapping))
 def main():
-    #tup()
-    List()
+    # tup()
+    # List()
+    Dict()
 
 
 if __name__ == '__main__':
